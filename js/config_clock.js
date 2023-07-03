df69dddd0e6eeefcf5700c3eb50774726ab1a31f
Enzo Villanueva <mp15265@gmail.com>
2023-07-03 19:18:49 -0300

    Opciones de apariencia

--- js/config_clock.js
+++ js/config_clock.js
@@ -2,6 +2,7 @@
 let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
+let double_point = document.getElementsByClassName("point");
 
 setInterval(() => {
     let horario = new Date();
@@ -10,3 +11,26 @@ setInterval(() => {
     minutes.innerHTML = (horario.getMinutes()<10 ? "0":"") + horario.getMinutes();
     seconds.innerHTML = (horario.getSeconds()<10 ? "0":"") + horario.getSeconds();
 }, 1000);
+
+document.getElementById("autumn").addEventListener("click",() => {
+    change_color_clock("black");
+    document.body.style.background  = "linear-gradient(45deg, #e76f51 40%, #f4a261, #e9c46a)";
+})
+
+document.getElementById("default").addEventListener("click",() => {
+    change_color_clock("white");
+    document.body.style.background  = "linear-gradient(45deg, #01283d 40%, #124559, #0a9396)";
+})
+
+document.getElementById("vice").addEventListener("click",() => {
+    change_color_clock("white");
+    document.body.style.background  = "linear-gradient(45deg, #7209b7 40%, #b5179e, #f72585)";
+})
+
+function change_color_clock(color){
+    hours.style.color = color;
+    minutes.style.color = color;
+    seconds.style.color = color;
+    double_point[0].style.color = color;
+    double_point[1].style.color = color;
+}
